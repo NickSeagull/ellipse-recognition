08ed141d033e901d0a20e77f01462109d4bfea96
Nikita Tchayka <nikitatchayka@gmail.com>
2015-09-30 19:26:27 +0100

    Population initialized to 1

--- spec/ellipse_recognition/core_spec.clj
+++ spec/ellipse_recognition/core_spec.clj
@@ -1,7 +1,13 @@
 (ns ellipse-recognition.core-spec
   (:require [speclj.core :refer :all]
-            [ellipse-recognition.core :refer :all]))
+            [ellipse-recognition.initialization :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "initialization"
+
+  (it "should give a population the maximum value of 1"
+      (let [population (initialize-population-with-size 100)]
+        (should=
+         (apply max (flatten population))
+         1)))
+
+  )
---
+++ src/ellipse_recognition/initialization.clj
@@ -0,0 +1,6 @@
+(ns ellipse-recognition.initialization)
+
+(defn initialize-population-with-size [size]
+  "Gives a random population based on the encoding
+  at the individual-parameters record"
+  [[1]])
