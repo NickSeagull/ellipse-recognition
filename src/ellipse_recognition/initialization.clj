f4b52120f869a488d2ce3d6d027675ff25550f54
Nikita Tchayka <nikitatchayka@gmail.com>
2015-09-30 20:41:22 +0100

    Change name of individual encoding

--- src/ellipse_recognition/initialization.clj
+++ src/ellipse_recognition/initialization.clj
@@ -1,17 +1,17 @@
 (ns ellipse-recognition.initialization)
 
-(def individual-parameters {:a     5
-                            :b     5
-                            :x0    6
-                            :y0    6
-                            :alpha 7})
+(def individual-bit-encoding {:a     5
+                              :b     5
+                              :x0    6
+                              :y0    6
+                              :alpha 7})
 
-(def individual-size (reduce + (vals individual-parameters)))
+(def individual-size (reduce + (vals individual-bit-encoding)))
 
 (defn- make-random-individual []
   (take individual-size (repeatedly #(rand-int 2))))
 
 (defn initialize-population-with-size [size]
   "Gives a random population based on the encoding
-  at the individual-parameters map"
+  at the individual-bit-encoding map"
   (take size (repeatedly #(make-random-individual))))
