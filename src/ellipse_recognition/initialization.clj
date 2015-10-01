767c2c2c7b2a75c36bd857ee0be54c27c24ae176
Nikita Tchayka <nikitatchayka@gmail.com>
2015-10-01 16:52:03 +0100

    WIP

--- src/ellipse_recognition/initialization.clj
+++ src/ellipse_recognition/initialization.clj
@@ -1,15 +1,8 @@
-(ns ellipse-recognition.initialization)
-
-(def individual-bit-encoding {:a     5
-                              :b     5
-                              :x0    6
-                              :y0    6
-                              :alpha 7})
-
-(def individual-size (reduce + (vals individual-bit-encoding)))
+(ns ellipse-recognition.initialization
+  (:require [ellipse-recognition.individual :refer :all]))
 
 (defn- make-random-individual []
-  (take individual-size (repeatedly #(rand-int 2))))
+  (take get-individual-size (repeatedly #(rand-int 2))))
 
 (defn initialize-population-with-size [size]
   "Gives a random population based on the encoding
