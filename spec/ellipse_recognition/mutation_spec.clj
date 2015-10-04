0c550a6e3d6f83e3916fb26019ab6ce848106fb6
Nikita Tchayka <nikitatchayka@gmail.com>
2015-10-04 17:54:00 +0100

    GREEN: Mutation with 'manual' probability

--- spec/ellipse_recognition/mutation_spec.clj
+++ spec/ellipse_recognition/mutation_spec.clj
@@ -6,4 +6,8 @@
           (it "makes a bit-string mutation"
               (let [population [[1 1 0] [0 0 1] [1 1 1] [0 0 0]]]
                 (should= (count (bit-string-mutate population))
+                         (count population))))
+          (it "also mutates with an assigned probability"
+              (let [population [[1 1 0] [0 0 1] [1 1 1] [0 0 0]]]
+                (should= (count (mutate population 0.5))
                          (count population)))))
--- src/ellipse_recognition/mutation.clj
+++ src/ellipse_recognition/mutation.clj
@@ -7,5 +7,12 @@
   (if (< (rand) (/ 1 population-size))
     (flip-bit (rand-int (count individual)) individual)))
 
+(defn- mutate-individual [probability individual]
+  (if (< (rand) probability)
+    (flip-bit (rand-int (count individual)) individual)))
+
 (defn bit-string-mutate [population]
-  (map (partial bit-string-mutate-individual (count population)) population))
+  (map (partial mutate-individual (/ 1 (count population))) population))
+
+(defn mutate [population probability]
+  (map (partial mutate-individual probability) population))
