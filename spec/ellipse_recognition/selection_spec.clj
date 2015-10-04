f1132e5fe1e800a2ba54e5f87d586d8fbf965c41
Nikita Tchayka <nikitatchayka@gmail.com>
2015-10-04 16:52:58 +0100

    GREEN: Selection by truncation

--- spec/ellipse_recognition/selection_spec.clj
+++ spec/ellipse_recognition/selection_spec.clj
@@ -7,9 +7,16 @@
 
 (describe "selection"
           (it "can select by elitist selection"
-              (let [population (initialize-population-with-size 10)
+              (let [population   (initialize-population-with-size 10)
                     image-matrix (load-image-as-binary-matrix "resources/coins.jpg" 0.5)
-                    scores     (evaluate population image-matrix)]
+                    scores       (evaluate population image-matrix)]
                 (should=
                  (count (select-by-elitism 2 population scores))
+                 2)))
+          (it "can also select by truncation"
+              (let [population   (initialize-population-with-size 10)
+                    image-matrix (load-image-as-binary-matrix "resources/coins.jpg" 0.5)
+                    scores       (evaluate population image-matrix)]
+                (should=
+                 (count (select-by-truncation 20 population scores))
                  2))))
--- src/ellipse_recognition/selection.clj
+++ src/ellipse_recognition/selection.clj
@@ -5,3 +5,12 @@
 
 (defn select-by-elitism [selection-size population scores]
   (subvec (sort-population population scores) 0 selection-size))
+
+(defn- get-size-by-percentage [percentage population]
+  (* (count population) (/ percentage 100)))
+
+(defn select-by-truncation [selection-percentage population scores]
+  (select-by-elitism
+   (get-size-by-percentage selection-percentage population)
+   population
+   scores))
